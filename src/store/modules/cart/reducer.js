deeedb2184692bad0bb30c702d9086c0cc3b8ff2
Fabio <techbrasilia@outlook.com>
2020-04-18 20:58:35 -0300

    finalizar com arquitetura flux

--- src/store/modules/cart/reducer.js
+++ src/store/modules/cart/reducer.js
@@ -2,19 +2,23 @@ import produce from 'immer';
 
 export default function cart(state = [], action) {
   switch (action.type) {
-    case '@cart/ADD':
+    case '@cart/ADD_SUCCESS':
       // return [...state, {...action.product, amount: 1, }, ];
       return produce(state, (draft) => {
-        const productIndex = draft.findIndex((p) => p.id === action.product.id);
+        const { product } = action;
 
-        if (productIndex >= 0) {
-          draft[productIndex].amount += 1;
-        } else {
-          draft.push({
-            ...action.product,
-            amount: 1,
-          });
-        }
+        draft.push(product);
+
+        // const productIndex = draft.findIndex((p) => p.id === action.product.id);
+
+        // if (productIndex >= 0) {
+        //   draft[productIndex].amount += 1;
+        // } else {
+        //   draft.push({
+        //     ...action.product,
+        //     amount: 1,
+        //   });
+        // }
       });
     case '@cart/REMOVE':
       return produce(state, (draft) => {
@@ -25,10 +29,11 @@ export default function cart(state = [], action) {
         }
       });
 
-    case '@cart/UPDATE_AMOUNT': {
-      if (action.amount <= 0) {
-        return state;
-      }
+    case '@cart/UPDATE_AMOUNT_SUCCESS': {
+      // ja está no saga
+      // if (action.amount <= 0) {
+      //   return state;
+      // }
 
       return produce(state, (draft) => {
         const productIndex = draft.findIndex((p) => p.id === action.id);
